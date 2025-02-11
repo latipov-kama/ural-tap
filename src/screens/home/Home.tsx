4644b9e5917be032fa61b198c98b5cb98c6b404d
KamaProgman <latipovkamran1@gmail.com>
2025-02-11 18:35:37 +0500

    test

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -9,23 +9,6 @@ import LevelProgress from "../../components/level-progress/LevelProgress";
 import { sendAuthData } from "../../services/auth/auth";
 import { TelegramInitData } from "../../types";
 
-// const data = {
-//   "user": {
-//     "id": 909990269,
-//     "first_name": "Kamran",
-//     "last_name": "",
-//     "username": "latipov_kama",
-//     "language_code": "ru",
-//     "allows_write_to_pm": true,
-//     "photo_url": "https://t.me/i/userpic/320/Jgo_S36x4Mww1tqsAYlTU4q-Eh4U4NjScTy0jANiS8Q.svg"
-//   },
-//   "chat_instance": "8180690052102929318",
-//   "chat_type": "sender",
-//   "auth_date": "1738578480",
-//   "signature": "yexIMgh2Z7zG_jaSxBhCpUbGEy8SgihWAvTaWn6ay6X-7O0C6Iut5FhGXRoRTFd885JQILrkm8g62-4VwKbUBw",
-//   "hash": "35dcd936e488bb2a16ab71e2d4d3d0426747a4d67cf2096377f0310a045dcf92"
-// }
-
 function Home() {
   const { coins, level, addCoins, levels } = useScoreStore();
   const [taps, setTaps] = useState(500);
--- src/services/auth/auth.ts
+++ src/services/auth/auth.ts
@@ -1,24 +1,50 @@
 import axios from "axios";
 
+const test = {
+  user: {
+    added_to_attachment_menu: false,
+    allows_write_to_pm: false,
+    first_name: "user-first-name",
+    id: 422,
+    is_bot: true,
+    is_premium: false,
+    language_code: "en",
+    last_name: "user-last-name",
+    photo_url: "user-photo",
+    username: "user-username",
+  },
+  auth_date: "1739220101",
+  signature: "",
+  hash: "ae79eca87f4580895f1b508f0944e5cff01d6b89c9d2a18a0a790439c7075323",
+};
+
 export const sendAuthData = async () => {
   if (typeof window === "undefined" || !window.Telegram?.WebApp) {
     alert("Telegram WebApp API is not available");
     return;
   }
 
-  const initData = window.Telegram.WebApp.initData;
-  if (!initData) {
-    alert("No initData found");
-    return;
-  }
-
   try {
+    // Кодируем объект в `application/x-www-form-urlencoded`
+    const encodedData = new URLSearchParams({
+      user: JSON.stringify(test.user),
+      auth_date: test.auth_date,
+      signature: test.signature,
+      hash: test.hash,
+    }).toString();
+
+    // Создаем JSON с `data`
+    const requestData = { data: encodedData };
+
+    console.log("Request JSON:", JSON.stringify(requestData));
+
+    // Отправляем JSON
     const response = await axios.post(
       `${import.meta.env.VITE_SERVER_URL}/auth/login`,
-      new URLSearchParams({ data: initData }).toString(), // Преобразуем в строку запроса
+      requestData,
       {
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Type": "application/json",
         },
       }
     );
