d59cc1e255bf97c9d9112dc7b9ee97c72c6a8f1e
KamaProgman <latipovkamran1@gmail.com>
2025-02-16 23:17:38 +0500

    test

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,13 +2,12 @@ import { create } from "zustand";
 import { fetchUserData, sendAuthData } from "../services/auth";
 import { User } from "../types/user";
 import { retrieveLaunchParams } from "@telegram-apps/sdk";
-// import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 interface AuthState {
   user: User | null;
   userId: number | null;
   isAuthenticated: boolean;
-  referralCode: string | null
+  referralCode: string | null;
   initAuth: () => Promise<void>;
 }
 
@@ -17,30 +16,32 @@ export const useAuthStore = create<AuthState>((set) => ({
   userId: null,
   user: null,
   referralCode: typeof window !== "undefined" ? localStorage.getItem("referral_code") : null,
+
   initAuth: async () => {
     try {
-      const { initDataRaw } = retrieveLaunchParams();
-      if (!initDataRaw) return;
+      let initDataRaw: string = "user=%7B%22id%22%3A909990269%2C%22first_name%22%3A%22Kamran%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22latipov_kama%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FJgo_S36x4Mww1tqsAYlTU4q-Eh4U4NjScTy0jANiS8Q.svg%22%7D&chat_instance=-1537647949971518884&chat_type=private&auth_date=1739711102&signature=i1YM7FDBa-EOqQpzzcQY520cNDqNWWYr7jnRjNh6Kpt-NHCXnAEKlCStC-l6k8SrWucfWCEoLGc24gtlkgYGCA&hash=e538c03c7ff743b9cd858b341f45b4efbfd3c602a170c2808f98a7710dc47969";
+      if (typeof window !== "undefined") {
+        const { initDataRaw: rawData } = retrieveLaunchParams();
+        initDataRaw = rawData ?? initDataRaw;
+      }
 
-      const searchParams = new URLSearchParams(window.location.search)
-      const referralCode = searchParams.get("start")
-      console.log(referralCode);
+      const searchParams = new URLSearchParams(window.location.search);
+      const referralCode = searchParams.get("start");
 
       if (referralCode) {
         localStorage.setItem("referral_code", referralCode);
         set({ referralCode });
       }
 
-      // const initDataRaw = "user=%7B%22id%22%3A909990269%2C%22first_name%22%3A%22Kamran%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22latipov_kama%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FJgo_S36x4Mww1tqsAYlTU4q-Eh4U4NjScTy0jANiS8Q.svg%22%7D&chat_instance=-1537647949971518884&chat_type=private&auth_date=1739711102&signature=i1YM7FDBa-EOqQpzzcQY520cNDqNWWYr7jnRjNh6Kpt-NHCXnAEKlCStC-l6k8SrWucfWCEoLGc24gtlkgYGCA&hash=e538c03c7ff743b9cd858b341f45b4efbfd3c602a170c2808f98a7710dc47969"
-      alert(JSON.stringify(initDataRaw))
+      alert(JSON.stringify(initDataRaw));
       const response = await sendAuthData(initDataRaw, referralCode);
 
       if (response?.userId) {
         set({ userId: response.userId });
 
-        const userData = await fetchUserData(response.userId)
+        const userData = await fetchUserData(response.userId);
         if (userData) {
-          set({ user: userData, isAuthenticated: true })
+          set({ user: userData, isAuthenticated: true });
 
           localStorage.removeItem("referral_code");
           set({ referralCode: null });
@@ -51,3 +52,6 @@ export const useAuthStore = create<AuthState>((set) => ({
     }
   },
 }));
+
+
+// const initDataRaw = "user=%7B%22id%22%3A909990269%2C%22first_name%22%3A%22Kamran%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22latipov_kama%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FJgo_S36x4Mww1tqsAYlTU4q-Eh4U4NjScTy0jANiS8Q.svg%22%7D&chat_instance=-1537647949971518884&chat_type=private&auth_date=1739711102&signature=i1YM7FDBa-EOqQpzzcQY520cNDqNWWYr7jnRjNh6Kpt-NHCXnAEKlCStC-l6k8SrWucfWCEoLGc24gtlkgYGCA&hash=e538c03c7ff743b9cd858b341f45b4efbfd3c602a170c2808f98a7710dc47969"
