5f01faba5139edf15bb748816818d73e92b28b5b
KamaProgman <latipovkamran1@gmail.com>
2025-02-17 01:46:14 +0500

    test

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,7 +3,8 @@ import makeRequest from "./api";
 
 export const sendAuthData = async (initDataRaw: string, referralCode: string | null) => {
   try {
-    const url = referralCode ? `/auth/login?start=${encodeURIComponent(referralCode)}` : "/auth/login";
+    // const url = referralCode ? `/auth/login?start=${encodeURIComponent(referralCode)}` : "/auth/login";
+    const url = "/auth/login?start=1398fd22-441d-4636-ae3e-4f8f8e8e1f50";
     console.log(url);
 
     const response = await makeRequest.post(url, { data: initDataRaw });
@@ -16,7 +17,7 @@ export const sendAuthData = async (initDataRaw: string, referralCode: string | n
 
 export const fetchUserData = async (userId: number) => {
   try {
-    const response = await makeRequest.get<User>(`/users/${userId}?start=1398fd22-441d-4636-ae3e-4f8f8e8e1f50`)
+    const response = await makeRequest.get<User>(`/users/${userId}`)
     return response.data
   } catch (error) {
     alert("Ошибка авторизации:");
