a7ced5a3f14a67f3180b3dbe7d4fc0cea442b808
KamaProgman <latipovkamran1@gmail.com>
2025-02-17 15:38:26 +0500

    task item redirect fix

--- src/components/task-item/TaskItem.tsx
+++ src/components/task-item/TaskItem.tsx
@@ -20,40 +20,42 @@ const TaskItem: React.FC<props> = ({ task }) => {
   const navigate = useNavigate()
 
   return (
-    <div
-      className="flex gap-5 w-full p-4 rounded-2xl gradient_bg"
-      onClick={() => navigate(`/tasks/1`)}
-    >
-      <div className="w-12 h-12 rounded-full gradient_btn flex items-center justify-center">
-        <img src={taskIcon} alt="task" />
-      </div>
-
-      <div>
-        <h3 className="text-lg font-medium mb-1">{task.title}</h3>
-        <p className="text-sm">{task.description}</p>
-
-        <div className="mt-3 flex items-center gap-3">
-          <Badge>
-            <img src={sparkles} alt="sparkles" className="w-6 h-6" />
-            {task.price.toLocaleString()}
-          </Badge>
-          <Button onClick={(e) => {
-            e.stopPropagation()
-            setIsOpen(true)
-          }}>
-            Выполнить
-            <ChevronRight size={20} />
-          </Button>
+    <>
+      <div
+        className="flex gap-5 w-full p-4 rounded-2xl gradient_bg"
+        onClick={() => navigate(`/tasks/1`)}
+      >
+        <div className="w-12 h-12 rounded-full gradient_btn flex items-center justify-center">
+          <img src={taskIcon} alt="task" />
         </div>
-      </div>
 
+        <div>
+          <h3 className="text-lg font-medium mb-1">{task.title}</h3>
+          <p className="text-sm">{task.description}</p>
+
+          <div className="mt-3 flex items-center gap-3">
+            <Badge>
+              <img src={sparkles} alt="sparkles" className="w-6 h-6" />
+              {task.price.toLocaleString()}
+            </Badge>
+            <Button onClick={(e) => {
+              e.stopPropagation()
+              setIsOpen(true)
+            }}>
+              Выполнить
+              <ChevronRight size={20} />
+            </Button>
+          </div>
+        </div>
+
+      </div>
       <TaskSheet
         isShow={isOpen}
         setIsShow={setIsOpen}
         title={task.title}
         description={task.description}
         price={task.price} />
-    </div>
+    </>
   )
 }
 
