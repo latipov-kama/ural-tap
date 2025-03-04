086f183d13afc3fcdfcdeee0f69928a2e7b152b6
KamaProgman <latipovkamran1@gmail.com>
2025-03-04 13:45:47 +0500

    fix

--- src/hooks/query/tasks.ts
+++ src/hooks/query/tasks.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 import { getTasks } from "../../services/tasksApi"
 import { Task } from "../../types/tasks"
 
