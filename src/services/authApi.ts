07297ba2d4f197b8cb22cab75189f2cc79ad9100
KamaProgman <latipovkamran1@gmail.com>
2025-03-14 14:56:17 +0500

    test

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -3,13 +3,11 @@ import makeRequest from "./api";
 
 export const sendAuthData = async (initDataRaw: string, referralCode: string | null = null) => {
   try {
-    console.log(referralCode)
-    const url = referralCode ? `/auth/login?startapp=${referralCode}` : "/auth/login";
+    const url = referralCode ? `/auth/login?startapp=${encodeURIComponent(referralCode)}` : "/auth/login";
 
     alert(url);
 
     const response = await makeRequest.post(url, { data: initDataRaw });
-    console.log(response)
     return response.data;
   } catch (error) {
     alert(`${error}`);
