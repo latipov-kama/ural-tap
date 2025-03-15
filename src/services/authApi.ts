46fcd3e470ce034ba8955f7ec7a2e92c900671ab
KamaProgman <latipovkamran1@gmail.com>
2025-03-16 00:53:16 +0500

    fix auth

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -1,13 +1,32 @@
+import { AxiosError } from "axios";
 import { User } from "../types/user";
 import makeRequest from "./api";
 
-export const sendAuthData = async (initDataRaw: string, referralCode: string | null = null) => {
-  try {
-    const url = referralCode ? `/auth/login?startapp=${encodeURIComponent(referralCode)}` : "/auth/login";
+interface AuthResponse {
+  userId: number
+}
 
-    const response = await makeRequest.post(url, { data: initDataRaw });
+export const sendAuthData = async (
+  initDataRaw: string,
+  referralCode: string | null = null
+): Promise<AuthResponse> => {
+  try {
+    const url = referralCode
+      ? `/auth/login?startapp=${encodeURIComponent(referralCode)}`
+      : "/auth/login";
+
+    const response = await makeRequest.post<AuthResponse>(url, { data: initDataRaw });
     return response.data;
   } catch (error) {
+    if (error instanceof Error) {
+      console.error("Ошибка авторизации:", error.message);
+    }
+
+    if (error instanceof AxiosError && error.response?.status === 400) {
+      console.warn("Повторная попытка входа с реферальным кодом. Перенаправляем на обычный логин...");
+      return sendAuthData(initDataRaw, null); // Пробуем без реферального кода
+    }
+
     alert(`${error}`);
     throw error;
   }
