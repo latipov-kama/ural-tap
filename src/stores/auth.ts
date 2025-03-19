5b9eedbd74f51b9fbd5477eb3fbab29389d8a1a4
KamaProgman <latipovkamran1@gmail.com>
2025-03-19 16:19:23 +0500

    fix

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { fetchUserData, sendAuthData } from "../services/authApi";
 import { User } from "../types/user";
-// import { retrieveLaunchParams } from "@telegram-apps/sdk";
+import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 interface AuthState {
   user: User | null;
@@ -17,19 +17,19 @@ export const useAuthStore = create<AuthState>((set) => ({
 
   initAuth: async () => {
     try {
-      // let referralCode: string | null = null;
-      const initDataRaw = "user=%7B%22added_to_attachment_menu%22%3Afalse%2C%22allows_write_to_pm%22%3Afalse%2C%22first_name%22%3A%22user-first-name%22%2C%22id%22%3A8489985%2C%22is_bot%22%3Atrue%2C%22is_premium%22%3Afalse%2C%22language_code%22%3A%22en%22%2C%22last_name%22%3A%22user-last-name%22%2C%22photo_url%22%3A%22user-photo%22%2C%22username%22%3A%22user-username%22%7D&auth_date=1742380263&signature=&hash=39e29fb120e840e1c4fc46eeb7ab9fc8d89b3e792716cf03bbae87d11b96e152"
+      let referralCode: string | null = null;
+      // const initDataRaw = "user=%7B%22added_to_attachment_menu%22%3Afalse%2C%22allows_write_to_pm%22%3Afalse%2C%22first_name%22%3A%22user-first-name%22%2C%22id%22%3A8489985%2C%22is_bot%22%3Atrue%2C%22is_premium%22%3Afalse%2C%22language_code%22%3A%22en%22%2C%22last_name%22%3A%22user-last-name%22%2C%22photo_url%22%3A%22user-photo%22%2C%22username%22%3A%22user-username%22%7D&auth_date=1742380263&signature=&hash=39e29fb120e840e1c4fc46eeb7ab9fc8d89b3e792716cf03bbae87d11b96e152"
 
-      // const { initDataRaw, startParam } = retrieveLaunchParams();
+      const { initDataRaw, startParam } = retrieveLaunchParams();
 
-      // if (startParam) {
-      //   referralCode = startParam;
-      // } else {
-      //   const initParams = new URLSearchParams(Telegram.WebApp.initData);
-      //   referralCode = initParams.get("start") || null;
-      // }
+      if (startParam) {
+        referralCode = startParam;
+      } else {
+        const initParams = new URLSearchParams(Telegram.WebApp.initData);
+        referralCode = initParams.get("start") || null;
+      }
 
-      // console.log(referralCode);
+      console.log(referralCode);
 
       if (!initDataRaw) {
         alert("initDataRaw не найдено, авторизация невозможна");
