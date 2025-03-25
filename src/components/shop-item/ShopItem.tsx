f4076ae5da3d201c6f6ac0adfc235e81b3a80a0d
KamaProgman <latipovkamran1@gmail.com>
2025-03-25 19:18:43 +0500

    shop item fix

--- src/components/shop-item/ShopItem.tsx
+++ src/components/shop-item/ShopItem.tsx
@@ -14,30 +14,29 @@ const ShopItem: React.FC<props> = ({ item, handleOpen }) => {
   return (
     <div className="gradient_bg p-4 flex flex-col rounded-2xl">
 
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between gap-2">
         <div className="gradient_circle w-10 h-10 rounded-full flex items-center justify-center">
           <img src={repeat} alt="repeat" className="w-7 h-7" />
-
         </div>
-        <Badge className="!px-2 !py-1 text-xs text-secondary bg-primary">
-          Осталось: 2 дня
+        <Badge className="!px-2 !py-1 text-xs text-secondary bg-primary text-balance">
+          до 28.03
         </Badge>
       </div>
 
       <p className="mt-4 text-lg leading-4 font-medium">
         {item.title}
       </p>
-      <p className="mt-1 text-sm text-secondary">
+      <p className="mt-1 text-xs text-secondary">
         {item.description}
       </p>
-
       <div className="mt-3 flex items-center gap-1">
         <img src={sparkles} alt="sparkles" className="w-5 h-5" />
         <span className="text-sm">{item.cost.toLocaleString()}</span>
       </div>
 
+
       <Button
-        className="mt-3 !py-2 !px-4 flex justify-center"
+        className="mt-3 py-1.5 flex justify-center text-xs"
         onClick={() => handleOpen(item)}
       // disabled={isActive}
       >
