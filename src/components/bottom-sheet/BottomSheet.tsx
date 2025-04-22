8df28784d6daa9cd73d81e2a3729ba2bea959f8a
KamaProgman <latipovkamran1@gmail.com>
2025-04-23 00:09:52 +0500

    sheet fix

--- src/components/bottom-sheet/BottomSheet.tsx
+++ src/components/bottom-sheet/BottomSheet.tsx
@@ -29,23 +29,23 @@ const BottomSheet = <T extends SheetItem>({ isShow, setIsShow, item, actionLabel
           />
 
           <motion.div
-            className='gradient_sheet w-full h-[58%] py-8 px-5 pb-28 rounded-t-2xl fixed bottom-0 left-0 z-30'
+            className='gradient_sheet w-full min-h-[58%] py-8 px-5 pb-28 rounded-t-2xl fixed bottom-0 left-0 z-30'
             initial={{ y: "100%" }}
             animate={{ y: 0 }}
             exit={{ y: "100%" }}
             transition={{ duration: 0.2, ease: "easeInOut" }}
           >
             <div className='h-full flex flex-col items-center justify-between'>
-              <div className="w-[72px] h-[72px] rounded-full gradient_btn flex items-center justify-center">
-                {item.image && <img src={item.image?.url} alt={item.title} className='w-11' />}
+              <div className="w-[72px] h-[72px] rounded-full overflow-hidden gradient_btn flex items-center justify-center mb-4">
+                {item.image && <img src={item.image?.url} alt={item.title} className='w-full h-full object-cover' />}
               </div>
 
-              <div className='text-center'>
+              <div className='text-center mb-6'>
                 <h3 className="text-xl font-medium mb-2">{item.title}</h3>
                 <p className="text-sm text-secondary">{item.description}</p>
               </div>
 
-              <Badge>
+              <Badge className='mb-4'>
                 <img src={sparkles} alt="sparkles" className="w-6 h-6" />
                 {item.reward && item.reward.toLocaleString()}
                 {item.cost && item.cost.toLocaleString()}
